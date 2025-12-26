ade39ee29deeee2590b7768a3b6c0e3be4fcaa52
ButrosV <peteris_veits@yahoo.com>
2025-12-26 17:21:23 +0200

    updated eda 'value_streaks' function to handle no-NaN values

--- src/utils/eda.py
+++ src/utils/eda.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from typing import Any
 
 
 def top_correlations(data: pd.DataFrame, corr_threshold:float=0.95) -> pd.Series:
@@ -17,36 +19,45 @@ def top_correlations(data: pd.DataFrame, corr_threshold:float=0.95) -> pd.Series
 
 
 
-def nan_streaks(data: pd.DataFrame, column: str, run_threshold: int = 0
+def value_streaks(data: pd.DataFrame, column: str, value: Any, run_threshold: int = 0
                     ) -> pd.DataFrame:
     """
-    Identify and return streaks of NaN values in a specified column exceeding a minimum length threshold.
+    Identify and return streaks of specified values (including NaN) in a specified 
+    column exceeding a minimum length threshold.
     
     **Usage Example:**
     ```
-    result = nan_streaks(df, "ndvi_ne", run_threshold=5)
-    df["ndvi_ne"].iloc[result["first_pos"]:result["last_pos"] + 1]  # Extract 2nd NaN streak[1]
+    # Check for streaks of 0
+    result = value_streaks(df, "total_cases", 0, run_threshold=5)
+    result = value_streaks(df, "ndvi_ne", run_threshold=5)
+    df["ndvi_ne"].iloc[result["first_pos"]:result["last_pos"] + 1]  # Extract 2nd NaN streak
     ```
     
     :param data: pandas DataFrame containing the input dataset.
     :param column: Name of the column to analyze for NaN streaks.
+    :param value: Value to detect streaks of (use `np.nan` for NaN streaks).
     :param run_threshold: Minimum streak length to include. Default is 0 (includes all streaks).
-    :return: A pandas DataFrame with columns 'first_pos', 'last_pos', and 'streak_len' for qualifying NaN streaks, sorted by length descending.
+    :return: A pandas DataFrame with columns 'first_pos', 'last_pos', and 'streak_len' 
+                for qualifying NaN streaks, sorted by length descending.
     """
-    null_sequences = data[column].isnull()
-    run_boundaries = null_sequences != null_sequences.shift()  # shift by one  + `!=` indicates where runs of equal values start/end
+    if pd.isna(value):
+        mask_sequences = data[column].isna()
+    else:
+        mask_sequences = data[column] == value
+    
+    run_boundaries = mask_sequences != mask_sequences.shift()  # shift by one  + `!=` indicates where runs of equal values start/end
     run_boundaries = run_boundaries.cumsum()  # asign numeric value to each run sequence (previous sequence+=1)
-    nan_run_boundaries = run_boundaries[null_sequences]  # filter nan == True run labels
-    run_groups = nan_run_boundaries.groupby(by=nan_run_boundaries)  # group these NaN sequences
+    sequence_run_boundaries = run_boundaries[mask_sequences]  # filter mask == True value run labels
+    run_groups = sequence_run_boundaries.groupby(by=sequence_run_boundaries)  # group these value sequences
 
-    nan_streak_df = run_groups.agg(
+    sequence_streak_df = run_groups.agg(
                         first_pos = lambda x: x.index[0],
                         last_pos = lambda x: x.index[-1],
-                        streak_len = "size"  # counts rows per group, similar to len, but mre stable with NaNs
+                        streak_len = "size"  # counts rows per group, similar to len, but mre stable if NaNs in data
                         )
-    nan_streak_df = nan_streak_df.query(f"streak_len > {run_threshold}")  #filter aggregated DatFrame
-    nan_streak_df = nan_streak_df.sort_values(by="streak_len", ascending=False)
-    nan_streak_df = nan_streak_df.reset_index(drop=True)  # drop now meaningless `run_boundaries` index
+    sequence_streak_df = sequence_streak_df.query(f"streak_len > {run_threshold}")  #filter aggregated DatFrame
+    sequence_streak_df = sequence_streak_df.sort_values(by="streak_len", ascending=False)
+    sequence_streak_df = sequence_streak_df.reset_index(drop=True)  # drop now meaningless `run_boundaries` index
 
-    return nan_streak_df
+    return sequence_streak_df
     
