2420cec441c25e61a7350fb65448a2b8091582c4
ButrosV <peteris_veits@yahoo.com>
2026-01-07 19:04:00 +0200

    added NaN imputation with city+weekofyear median

--- src/preprocessing/clean.py
+++ src/preprocessing/clean.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from typing import List
+
 
 def drop_nan_rows(X: pd.DataFrame, y: pd.Series | None = None,
                   threshold_percent: float = 0.5):
@@ -11,8 +13,32 @@ def drop_nan_rows(X: pd.DataFrame, y: pd.Series | None = None,
     :return: Filtered X (and y if provided), both with reset_index().
     """
     row_drop_threshold = int(len(X.columns) * threshold_percent)
-    result = X.dropna(thresh=row_drop_threshold)
+    X_no_nan = X.dropna(thresh=row_drop_threshold)
     if y is not None:
-        return result.reset_index(), y.iloc[result.index].reset_index()
-    return result.reset_index()
+        return X_no_nan.reset_index(), y.iloc[X_no_nan.index].reset_index()
+    return X_no_nan.reset_index()
+
+
+def median_groupwise_impute(X: pd.DataFrame,
+                            group_keys: List[str] = ['city', 'weekofyear']):
+    """
+    Impute NaN values in numeric columns using median within specified group keys.
+    
+    :param X: pandas DataFrame containing grouping columns and features to impute.
+    :param group_keys: List of column names for grouping. Default ['city', 'weekofyear'].
+    :return: Copy of input DataFrame with NaNs filled by group-wise medians.
+    """
+    missing_keys = set(group_keys) - set(X.columns)
+    if missing_keys:
+        raise ValueError(f"Missing group keys {missing_keys}")
+
+    X_no_nan = X.copy()
+    cols_with_nan = X_no_nan.select_dtypes(include="number")\
+        .columns[X_no_nan.select_dtypes(include="number").isna().sum() > 0].to_list()
+
+    if len(cols_with_nan) > 0:
+        X_no_nan[cols_with_nan] = X_no_nan[cols_with_nan + group_keys]\
+            .groupby(by=group_keys)[cols_with_nan]\
+            .transform(lambda group: group.fillna(group.median()))
+    return X_no_nan
     
